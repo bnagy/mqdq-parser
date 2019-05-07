42231509fb1f1bdb94fc0d2d1b6dd0f4addc3bf6
ben <git@ben.iagu.net>
2019-05-07 13:30:02 +0930

    convert comments to docstrings

--- mqdq/line_analyzer.py
+++ mqdq/line_analyzer.py
@@ -4,9 +4,16 @@ import re
 
 def classify_caesura(l, n, strict=False):
     
-    # Classify the caesura occurring in foot n
-    # In: l, a BeautifulSoup <line>; n, an Int
-    # Out: A String, Q, -, W, or S (Quasi, None, Weak, Strong)
+    """Classify the caesura occurring in foot n.
+
+    Args:
+        l (bs4 <line>): Line to classify
+        n (int): Foot number to classify (1..6)
+        strict (bool): If True, consider Quasi as None
+
+    Returns:
+        string: Q, -, W, or S (Quasi, None, Weak, Strong)
+    """
     
     if l['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
@@ -26,14 +33,38 @@ def classify_caesura(l, n, strict=False):
                     return '-'
             elif re.search('%d[Tc]' % n, w['sy']): 
                 # we've passed the end of the foot in question
-                break
-        return '-'
-
+                return '-'
     except:
         raise ValueError("Can't handle this: %s" % l)
   
+def grep(soup, s):
+
+    """Case insensite grep on the text contents.
+
+    Args:
+        soup (bs4 soup): The text in which to search
+        s (string): String to search for. This will be converted
+                    into a regular expression, so re characters
+                    are allowed.
+
+    Returns:
+        (list): a list of matching bs4 <line>s
+    """
+
+    r = re.compile(s, re.IGNORECASE)
+    return [s.parent.parent for s in soup.find_all(string=r)]
 
 def elision_after_foot(n, l):
+
+    """Is there elision after foot n?
+
+    Args:
+        n (int): Foot number to check (1..6)
+        l (bs4 <line>): Line to check
+
+    Returns:
+        (bool): The result
+    """
 
     if l['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
@@ -52,15 +83,17 @@ def elision_after_foot(n, l):
 
 def metrical_nucleus(l, strict=False):
 
-    # Classify the caesurae in the second, third and fourth feet (sometimes
-    # called 1.5, 2.5, 3.5). The possible values are:
-    # - '-' for None (foot falls completely inside a word)
-    # - 'Q' for Quasi (caesura would occur, but there is elision)
-    # - 'W' for Weak (caesura after the first breve in a trochee)
-    # - 'S' for Strong (caesura after the arsis)
-    #
-    # In: a <line>
-    # Out: a String of three characters
+    """Classify the caesurae in the second, third and fourth feet 
+    (sometimes called 1.5, 2.5, 3.5).
+
+    Args:
+        l (bs4 <line>): Line to operate on
+        strict (bool): If True, consider Quasi as None
+
+    Returns:
+        (string): A three element string, made up of the following:
+                  Q, -, W, or S (Quasi, None, Weak, Strong)
+    """
 
     return ''.join([
         classify_caesura(l,2,strict),
@@ -70,10 +103,15 @@ def metrical_nucleus(l, strict=False):
 
 def has_3rd_foot_caes(l, strict=True):
     
-    # Determine whether a line has any kind of caesura
-    # in the third foot
-    # In: a <line>
-    # Out: Boolean
+    """Determine whether a line has any kind of caesura in the third foot.
+    
+    Args:
+        l (bs4 <line>): Line to check
+        strict (bool): If True, accept Quasi caesurae (occurring over an elision)
+
+    Returns:
+        (bool): The result
+    """
     
     if l['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
@@ -93,9 +131,14 @@ def has_3rd_foot_caes(l, strict=True):
 
 def diaer_after_first(l):
 
-    # Determine whether a line has a diaeresis after the first foot.
-    # In: a <line>
-    # Out: Boolean
+    """Determine whether a line has a diaeresis after the first foot.
+    
+    Args:
+        l (bs4 <line>): Line to check
+
+    Returns:
+        (bool): The result
+    """
 
     if l['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
@@ -113,9 +156,14 @@ def diaer_after_first(l):
 
 def has_bd(line):
     
-    # Determine whether a given line has bucolic diaeresis.
-    # In: a <line>
-    # Out: Boolean
+    """Determine whether a line contains a bucolic diaeresis.
+    
+    Args:
+        l (bs4 <line>): Line to check
+
+    Returns:
+        (bool): The result
+    """
     
     if line['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
@@ -150,17 +198,21 @@ def _get_syls_with_stress(w):
 
 def txt(l, scan=False, number_with=None):
 
-    # Return the text from a line. If the scan option is True add another line
-    # formatted with the scansion below it (including elision and stress). Eg:
-    #
-    # Dixerat, atque illam media  inter talia   ferro
-    # 1A'1b1c  2A'_  2T'3A 3b'3c_ 4A'4T 5A'5b5c 6A'6X
-    #
-    # In: a <line>
-    # Out: a String
-    #
-    # NB: Numbering the lines with book:line references can be pretty slow, because
-    # we need to search the whole bs4 collection per line.
+    """Extract the text from a line.
+
+    Args:
+        l (bs4 <line): Line to operate on
+        scan (bool): If True, add a line of scansion and accent:
+                     Dixerat, atque illam media  inter talia   ferro
+                     1A'1b1c  2A'_  2T'3A 3b'3c_ 4A'4T 5A'5b5c 6A'6X
+
+        number_with (bs4 soup): If provided, text will be numbered by finding the lines
+                                in that bs4 object (WARNING! Can be slow!):
+                                8:196> Caede tepebat humus, foribusque affixa  superbis
+                                       1A'1b 1c2A'2b 2c'3A  3b3c4A'_   4T5A'5b 5c6A'6X WSQ
+    Returns:
+        (string): The result
+    """
 
     res = []
 
@@ -211,7 +263,19 @@ def txt(l, scan=False, number_with=None):
 
 def txt_and_number(ll, every=5, scan=False, start_at=1):
     
-    # We can't assume ll has been cleaned!
+    """Extract the text from a line, with numbers. Where `txt` uses references to the text,
+    this uses independent numbers. Use this method if you want to print out a
+    sequentially numbered extract. Use `txt` if you want to add numbers to the result of a
+    search or a random sample.
+
+    Args:
+        ll (list of bs4 <line>): Lines to operate on
+        every (int, default=5): How often to add numbers
+        start_at (int, default=1): Where to start the numbering
+
+    Returns:
+        (string): The result
+    """
 
     # the string length of the highest line number (100==3)
     n_len = len(str(len(ll)+start_at))
@@ -236,15 +300,50 @@ def txt_and_number(ll, every=5, scan=False, start_at=1):
     return numbered
 
 def which_book(l, soup):
+
+    """Determine which book in a bs4 soup contains a given line.
+
+    Args:
+        l (bs4 <line>): Line to check
+        soup (bs4 soup): Soup to check in
+
+    Returns:
+        (int): The index of the 'division' containing l (0-based) or None
+    """
+
     for d in soup('division'):
         if l in d:
             return d['title']
     return None
 
 def bookref(l, soup):
-    return("%2s:%-3d" % (which_book(l, soup), int(l['name'])))
+
+    """Return a formatted reference book:line for a given line and text.
+
+    Args:
+        l (bs4 <line): Line to use
+        soup (bs4 soup): Text to use for the numbering
+
+    Returns:
+        (string): The reference, eg 6:825 or None
+    """
+
+    b = which_book(l, soup)
+    if not b:
+        return None
+    return("%2s:%-3d" % (b, int(l['name'])))
 
 def clean(ll):
+
+    """Remove all corrupt lines from a set of bs4 <line>s
+
+    Args:
+        ll (list of bs4 <line>): Lines to clean
+
+    Returns:
+        (list of bs4 <line>): The lines, with the corrupt ones removed.
+    """
+
     return [l for l in ll if l['pattern']!='corrupt']
 
 def _stressed(w):
@@ -310,61 +409,66 @@ def _has_elision(w):
     # SY for synalepha
     return w.has_attr('mf') and w['mf']=='SY'
 
-def ictus_conflicts(line):
+def ictus_conflicts(l):
 
-    # Count the number of ictus conflicts in a line
-    #
-    # In: a <line>
-    # Out: Int, number of conflicts
+    """Count the number of ictus conflicts in a line, considering
+    every foot (not every word).
 
-    count = 0
-    for idx, w in enumerate(line('word')):
-        # don't process words with no syllables
-        if len(w['sy'])==0:
-            # no content at all, ignore
-            continue
-        if w.has_attr('mf') and w['mf'] == "PE":
-            # prodelision, fama est --> fama'st skip this word, it has no
-            # syllables left.
-            if len(w['sy']) != 0:
-                raise RuntimeError("BUG: Prodelision with content??")
-            continue
-        if _conflict(w, idx):
-            count+=1
+    Args:
+        l (bs4 <line>): Line to check
 
-    return count
+    Returns:
+        (int): Number of conflicts (0..6)
+    """
+
+    # Slightly magic. True counts as 1 in python, False as 0
+    return sum([conflict_in_foot(f,l) for f in range(1,7)])
 
 CLITICS = ('que', 'ne', 've')
 
-def conflict_in_foot(n, line):
+def conflict_in_foot(n, l):
 
-    if line['pattern'] == 'corrupt':
+    """Determine if the nth foot in the given line has an ictus/accent conflict
+
+    Args:
+        n (int): Foot to check (1..6)
+
+    Returns:
+        (bool): True if a conflict exists, False otherwise
+    """
+
+    if l['pattern'] == 'corrupt':
         raise ValueError("Can't operate on a corrupt line!")
 
-    containing_word = next((w for w in line('word') if re.search('%dA'%n, w['sy'])), None)
+    containing_word = next((w for w in l('word') if re.search('%dA'%n, w['sy'])), None)
     if not containing_word:
-        raise ValueError("No arsis for syllable %d in line?? %s" % (n, txt(line)))
+        raise ValueError("No arsis for syllable %d in line?? %s" % (n, txt(l)))
 
     return _conflict(containing_word, n)
 
-def ic_by_foot(line):
+def harmony(l):
 
-    # Calculate the ictus conflicts for the first four feet, not by word
-    #
-    # In: a <line>
-    # Out: a String of four syllables (Conflict or Harmony) 'CHHC'
+    """Calculate the ictus conflicts for the first four feet (since the final
+    two feet are almost always in harmony)
+    
+    Args:
+        l (bs4 <line): Line to check
 
-    if line['pattern'] == 'corrupt':
-        raise ValueError("Can't operate on a corrupt line!")
+    Returns:
+        (string): String of four characters (Conflict or Harmony) 'CHHC'
+    """
+
+    if l['pattern'] == 'corrupt':
+        raise ValueError("Can't operate on a corrupt l!")
 
     res = []
     try:
         for i in range(1,5): # (stops at 4 not 5)
-            if conflict_in_foot(i, line):
+            if conflict_in_foot(i, l):
                 res.append('C')
             else:
                 res.append('H')
 
         return ''.join(res)
     except:
-        raise ValueError("Error processing: %s" % line)
+        raise ValueError("Error processing: %s" % l)
