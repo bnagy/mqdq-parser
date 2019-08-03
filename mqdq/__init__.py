5ff4f3c2e9b72fa51282ae1a41964b5520d071bb
ben <git@ben.iagu.net>
2019-08-03 14:37:57 +0930

    huge update

--- mqdq/__init__.py
+++ mqdq/__init__.py
@@ -1,3 +1,4 @@
 from . import line_analyzer
 from . import counter_factory
 from . import metrics
+from . import utils
--- mqdq/line_analyzer.py
+++ mqdq/line_analyzer.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from collections import Counter
 import re
+import numpy as np
+import pandas as pd
 
 def classify_caesura(l, n, strict=False):
     
@@ -37,23 +39,6 @@ def classify_caesura(l, n, strict=False):
     except:
         raise ValueError("Can't handle this: %s" % l)
   
-def grep(soup, s):
-
-    """Case insensite grep on the text contents.
-
-    Args:
-        soup (bs4 soup): The text in which to search
-        s (string): String to search for. This will be converted
-                    into a regular expression, so re characters
-                    are allowed.
-
-    Returns:
-        (list): a list of matching bs4 <line>s
-    """
-
-    r = re.compile(s, re.IGNORECASE)
-    return [s.parent.parent for s in soup.find_all(string=r)]
-
 def elision_after_foot(n, l):
 
     """Is there elision after foot n?
@@ -107,7 +92,7 @@ def has_3rd_foot_caes(l, strict=True):
     
     Args:
         l (bs4 <line>): Line to check
-        strict (bool): If True, accept Quasi caesurae (occurring over an elision)
+        strict (bool): If True, don't accept Quasi caesurae (occurring over an elision)
 
     Returns:
         (bool): The result
@@ -194,163 +179,18 @@ def _get_syls_with_stress(w):
         return w['sy']
     syls = re.findall('..', w['sy'])
     stress = _stressed(w)
-    return( ''.join(syls[:stress+1]) + '\'' + ''.join(syls[stress+1:]))
-
-def txt(l, scan=False, number_with=None):
-
-    """Extract the text from a line.
-
-    Args:
-        l (bs4 <line): Line to operate on
-        scan (bool): If True, add a line of scansion and accent:
-                     Dixerat, atque illam media  inter talia   ferro
-                     1A'1b1c  2A'_  2T'3A 3b'3c_ 4A'4T 5A'5b5c 6A'6X
-
-        number_with (bs4 soup): If provided, text will be numbered by finding the lines
-                                in that bs4 object (WARNING! Can be slow!):
-                                8:196> Caede tepebat humus, foribusque affixa  superbis
-                                       1A'1b 1c2A'2b 2c'3A  3b3c4A'_   4T5A'5b 5c6A'6X WSQ
-    Returns:
-        (string): The result
-    """
-
-    res = []
-
-    try:
-            
-        words = l('word')
-
-        l_prefix = ''
-        scan_prefix = ''
-        if number_with:
-            l_prefix = bookref(l, number_with) + '> '
-            scan_prefix = ' '*len(l_prefix)
-
-        res=[]
-        if not scan:
-            return l_prefix + ' '.join([w.text for w in l('word')])
-        if l['pattern']=='corrupt':
-            return l_prefix + ' '.join([w.text for w in l('word')]) + "\n" + scan_prefix + "[corrupt]"
-
-        for w in words:
-            if len(w['sy'])==0:
-                if w.has_attr('mf') and w['mf']=='PE':
-                    # prodelision, the word is there but
-                    # the syllables vanished
-                    # Pollicita est -> Pollicit'est
-                    res.append(['_', w.text])
-                continue
-            syls = _get_syls_with_stress(w)
-            if _has_elision(w):
-                res.append([syls+'_',w.text])
-            else:
-                res.append([syls,w.text])
-            
-    except:
-        raise ValueError("Can't handle this: %s" % l)
-    
-    s1, s2='',''
-    # make the scan and the words line up, depending on which is longer.
-    for syls,txt in res:
-        if len(txt)>=len(syls):
-            s1 += txt + ' '
-            s2 += syls + ' '*(len(txt) - len(syls) + 1)
-        else:
-            s1 += txt + ' '*(len(syls) - len(txt) + 1)
-            s2 += syls + ' '
-    
-    return (l_prefix + s1.strip() + '\n' + scan_prefix + s2.strip())
-
-def txt_and_number(ll, every=5, scan=False, start_at=1):
-    
-    """Extract the text from a line, with numbers. Where `txt` uses references to the text,
-    this uses independent numbers. Use this method if you want to print out a
-    sequentially numbered extract. Use `txt` if you want to add numbers to the result of a
-    search or a random sample.
-
-    Args:
-        ll (list of bs4 <line>): Lines to operate on
-        every (int, default=5): How often to add numbers
-        start_at (int, default=1): Where to start the numbering
-
-    Returns:
-        (string): The result
-    """
-
-    # the string length of the highest line number (100==3)
-    n_len = len(str(len(ll)+start_at))
-    strs = [txt(l,scan) for l in ll]
-    numbered = []
-    for idx, s in enumerate(strs):      
-        if scan:
-            s1, s2 = s.splitlines()
-            if (idx+start_at)%every==0:
-                s1 = ("%*d  " % (n_len,idx+start_at)) + s1
-            else:
-                s1 = ' '*(n_len+2) + s1
-            s2 = ' '*(n_len+2) + s2
-            numbered.append('\n'.join([s1,s2]))
-        else:
-            if (idx+start_at)%every==0:
-                s = ("%*d  " % (n_len,idx+start_at)) + s
-            else:
-                s = ' '*(n_len+2) + s
-            numbered.append(s)
-
-    return numbered
-
-def which_book(l, soup):
-
-    """Determine which book in a bs4 soup contains a given line.
-
-    Args:
-        l (bs4 <line>): Line to check
-        soup (bs4 soup): Soup to check in
-
-    Returns:
-        (int): The index of the 'division' containing l (0-based) or None
-    """
-
-    for d in soup('division'):
-        if l in d:
-            return d['title']
-    return None
-
-def bookref(l, soup):
-
-    """Return a formatted reference book:line for a given line and text.
-
-    Args:
-        l (bs4 <line): Line to use
-        soup (bs4 soup): Text to use for the numbering
-
-    Returns:
-        (string): The reference, eg 6:825 or None
-    """
-
-    b = which_book(l, soup)
-    if not b:
-        return None
-    return("%2s:%-3d" % (b, int(l['name'])))
-
-def clean(ll):
-
-    """Remove all corrupt lines from a set of bs4 <line>s
-
-    Args:
-        ll (list of bs4 <line>): Lines to clean
-
-    Returns:
-        (list of bs4 <line>): The lines, with the corrupt ones removed.
-    """
-
-    return [l for l in ll if l['pattern']!='corrupt']
+    syls[stress] = '`'+syls[stress]
+    return ''.join(syls)
 
 def _stressed(w):
+
+    if w.text.lower() in _ACCENT_LAST_FOOT:
+        return -1
+
     syls = re.findall('..', w['sy'])
     
     # For words with elision and a long syllable ending, I assume the stress
-    #stayed on that syllable. This is based on analysing lots of hexameter poetry.
+    # stayed on that syllable. This is based on analysing lots of hexameter poetry.
     #
     # 3:464>  Dona dehinc auro grauia sectoque elephanto
     #         1A1b 1c2A   2T3A 3b3c4A 4T5A_    5b5c6A6X
@@ -364,7 +204,7 @@ def _stressed(w):
     # There's a pretty good chance that 1A is _not_ stressed
 
     if _has_elision(w) and re.match('.[ATX]',syls[-1]):
-        return len(syls)-1
+        return -1
 
     # For words ending with a short syllable, though, we ignore
     # the elision. Aeneid is 'packed' with lines like this:
@@ -378,28 +218,38 @@ def _stressed(w):
     # cf Allen, Vox Latina, or any of a million grammatici
 
     if len(syls) == 1:
-        return(0)
+        return 0 
     # Two syls, always first syllable
     elif len(syls) == 2:
-        return(0)
+        return 0
     # If three or more...
     else:
         # Second last syllable is long then it takes the stress
         # (in this markup, long syls are uppercase A, T or X
         if re.match('.[ATX]', syls[-2]):
-            return(len(syls)-2)
+            return -2
         # otherwise it's the syllable before that is stressed
         else:
-            return(len(syls)-3)
+            return -3
+
+# cf Allen, Vox Latina (1965), 87-8
+_UNACCENTED = {'at', 'ac', 'atque', 'et', 'sed', 'igitur', 'vel', 'aut', 'iam', 'seu', 'nam'}
+_ACCENT_LAST_FOOT = {'nostras', 'illic', 'adhuc', 'tanton', 'adduc'}
 
 def _conflict(w,foot):
 
     # Not only does the stress need to fall on an Arsis, but it needs to
     # fall on the arsis of the foot we're actually interested in.
 
+    # if a word has no stress, then the ictus is not stressed, so it's
+    # a conflict.
+    if w.text.lower() in _UNACCENTED:
+        return True
+
     syls = re.findall('..', w['sy'])
     if len(w['sy'])==0:
         raise ValueError("No syllables?")
+
     # A means arsis, which is the start of either a spondee or dactyl
     if syls[_stressed(w)] == ('%dA'%foot):
         return(False)
@@ -424,6 +274,21 @@ def ictus_conflicts(l):
     # Slightly magic. True counts as 1 in python, False as 0
     return sum([conflict_in_foot(f,l) for f in range(1,7)])
 
+def caps(l):
+
+    """Count the number of capital letters in a line. This can be 
+    useful for counting proper nouns, but is very sensitive to
+    the properties of your edition. Use with caution.
+
+    Args: 
+        l (bs4 <line>): Line to check
+    
+    Returns:
+        (int): Number of uppercase letters
+    """
+
+    return sum([x[0].isupper() for x in [w.text for w in l('word')]])
+
 CLITICS = ('que', 'ne', 've')
 
 def conflict_in_foot(n, l):
@@ -446,7 +311,200 @@ def conflict_in_foot(n, l):
 
     return _conflict(containing_word, n)
 
-def harmony(l):
+def predictors_by_foot(l):
+
+    """Return a list of the predictors for this line (by foot).
+    The things we know about are:
+    - Foot length (S or D)
+    - Foot ictus harmony (C or H)
+    - Foot caesura (S, W, Q, -)
+    - Foot bucolic diaeresis
+
+    We record the following per foot as a compound string
+    - 1 - length, harmony (4 levels)
+    - 2 - length, harmony, caesura (16 levels)
+    - 3 - length, harmony, caesura (16 levels)
+    - 4 - length, harmony, caesura, BD (32 levels)
+
+    Args:
+        l (bs4 <line>): Line to analyze
+
+    Returns:
+        (list): a list of four strings
+    """
+
+    if l['pattern'] == 'corrupt':
+        raise ValueError("Can't operate on a corrupt line!")
+    try:
+        l1, l2, l3, l4 = list(l['pattern'])[:4]
+        h1, h2, h3, h4 = list(harmony(l))
+        c2,c3,c4 = list(metrical_nucleus(l, strict=False))
+        bd4 = 'T' if has_bd(l) else 'F'
+
+        f1 = ''.join([l1,h1])
+        f2 = ''.join([l2,h2,c2])
+        f3 = ''.join([l3,h3,c3])
+        f4 = ''.join([l4,h4,c4,bd4])
+
+        return [f1,f2,f3,f4]
+    except:
+        raise ValueError("Error processing: %s" % l)
+
+def predictors_by_feature(l):
+
+    """Return a list of predictors for this line (by feature).
+    
+    The returned strings are:
+    - Quantity of each the first four feet (eg "SSDS")
+    - Harmony/Conflict of the first four feet (eg "CHCH")
+    - Caesurae in the 2nd, 3rd and 4th foot (eg "SW-")
+    - Whether the line has bucolic diaeresis ("T" or "F")
+    
+    Args:
+        l (bs4 <line>): Line to analyze
+
+    Returns:
+        (list): a list of four strings
+    """
+
+    if l['pattern'] == 'corrupt':
+        raise ValueError("Can't operate on a corrupt line!")
+    try:
+        f1 = l['pattern'][:4]
+        f2 = harmony(l)
+        f3 = metrical_nucleus(l, strict=False)
+        f4 = 'T' if has_bd(l) else 'F'
+
+        return [f1,f2,f3,f4]
+    except:
+        raise ValueError("Error processing: %s" % l)
+
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def _chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def _chunk_mean(df, n, round=True):
+
+    # - 'arange' NOT 'arrange' - a list [0..len(df)]
+    # ie group by the result of row_index div (integer division) n
+    # then apply mean() to each group
+
+    newdf = df.groupby(np.arange(len(df))//n).mean()
+    if round and len(df)%n != 0:
+        newdf = newdf[:-1]
+    return newdf
+
+def _funky_map(l, mappers):
+
+    # Each entry in mappers to consist of a lambda and a baseline
+    # The lambda should return something that can be coerced into
+    # a list.
+    # eg [lambda l: l['pattern'][:4], 'S']
+    # the lambda will return 'DDSS' and then each value is
+    # compared to the baseline ('S')
+    # final result would be [0,0,1,1]
+
+    res=[]
+    for fn, comp in mappers:
+        for x in list(fn(l)):
+            res.append(1 if x==comp else 0)
+    return res
+
+def _binary_features(l):
+    return _funky_map(l, 
+              [
+                  [lambda l: l['pattern'][:4], 'S'],
+                  [lambda l: harmony(l), 'C'],
+                  [lambda l: 'T' if has_bd(l) else 'F', 'T'],
+                  [lambda l: metrical_nucleus(l), 'S'],
+                  [lambda l: metrical_nucleus(l), 'W']
+              ])
+
+BINARY_FEATURES = ['F1S', 'F2S', 'F3S', 'F4S', 'F1C', 'F2C', 'F3C', 'F4C', 'BD', 'F2SC', 'F3SC', 'F4SC', 'F2WC', 'F3WC', 'F4WC']
+ALL_FEATURES = ['F1S', 'F2S', 'F3S', 'F4S', 'F1C', 'F2C', 'F3C', 'F4C', 'BD', 'F2SC', 'F3SC', 'F4SC', 'F2WC', 'F3WC', 'F4WC', 'SYN']
+BEST_FEATURES = ['F1S', 'SYN', 'F4S', 'F1C', 'F3SC', 'F3WC', 'F3C', 'F4C', 'F3S', 'F2C', 'F4SC']
+def chunked_features(ll, n, feats=ALL_FEATURES):
+
+    """Take a set of binary features per line, and return a chunked average.
+
+    Eg if the feature was F1S (first foot spondee), every line would be given
+    a 1 or 0, and the result would be the proportion of lines in the chunk with
+    that feature.
+
+    Current Features (15 in total)
+    - Spondee (feet 1 to 4)
+    - Ictus/Accent Conflict (1 to 4)
+    - Bucolic Diaeresis (foot 4)
+    - Strong Caesura (feet 2, 3, 4)
+    - Weak Caesura (feet 2, 3, 4)
+
+    NB: If the length of `ll` is not divisible by `n` the last chunk will still be
+        calculated, but the variance might be high. It's up to the user to drop that
+        chunk, if this is a problem.
+        
+    Args:
+        ll (list of bs4 <line>s): Lines to operate on
+        n (int): chunk size (1 .. len(ll))
+
+    Returns:
+        (pandas.DataFrame): The results, one per row, with a header row
+                            matching the predefined features
+    """
+
+    df = pd.DataFrame(map(lambda l: _binary_features(l), ll), columns=BINARY_FEATURES)
+    if 'SYN' in feats:
+        df['SYN'] = [elision_count(l) for l in ll]
+    return _chunk_mean(df[feats], n)
+
+def elision_count(l):
+
+    """Returns the number of elisions in a given line
+
+    Args:
+        l (a bs4 <line>): The line
+
+    Returns:
+        (int): The number of elisions
+    """
+
+    return(sum([(1 if _has_elision(w) else 0) for w in l('word')]))
+
+def distribution(ll, feats=ALL_FEATURES):
+
+    """Transforms the lines into the feature vectors, but doesn't chunk them. This
+    means that we have one observation per line. Since this transformation is the
+    slowest part of the process it can be useful to do this once before using methods
+    that sample or analyze multiple times.
+
+    Rather than calculating lots of these with different feature sets, it is probably
+    better to calculate it with ALL_FEATURES and then subset the DataFrame using []
+
+    Args:
+        ll (list of bs4 <line>s): Lines to operate on
+
+    Returns:
+        (pandas.DataFrame): The results, one per row, with a header row
+                            matching the predefined features
+    """
+
+    return chunked_features(ll, 1, feats)
+
+def centroid(ll, feats=ALL_FEATURES):
+
+    """Returns the centroid (vector average) for the given set of lines
+
+    Args:
+        ll (list of bs4 <line>s): Lines to operate on
+
+    Returns:
+        (pandas.DataFrame): A DF containing one row, the centroid
+    """
+
+    return chunked_features(ll, len(ll), feats)
+
+def harmony(l, n=4):
 
     """Calculate the ictus conflicts for the first four feet (since the final
     two feet are almost always in harmony)
@@ -463,7 +521,7 @@ def harmony(l):
 
     res = []
     try:
-        for i in range(1,5): # (stops at 4 not 5)
+        for i in range(1,n+1): # (stops at 4 not 5)
             if conflict_in_foot(i, l):
                 res.append('C')
             else:
