431d1282a5f0275a9f07ba9be233369f2cc7497d
ben <git@ben.iagu.net>
2019-08-05 14:45:40 +0930

    oops forgot these

---
+++ mqdq/mahalanobis.py
@@ -0,0 +1,179 @@
+from bs4 import BeautifulSoup
+import random
+from mqdq import line_analyzer as la
+from mqdq import utils
+import pandas as pd
+import numpy as np
+import scipy as sp
+from scipy.stats import chi2
+
+def explain(x, dist):
+
+    """Calculate the Mahalanobis distance of a vector x from
+    the distribution dist. This also returns a contribution vector
+    which shows how much each feature contributes to the distance.
+
+    You may notice that some features in that vector have negative values.
+    The vector itself will always sum to a non-negative value, because
+    of the nature of the calculation (the co-variance matrix is positive
+    semi-definite). The negative value features occur when there is
+    correlation. For example, if feature A scores contributes 20 to the
+    distance, but it is correlated with feature B, then B might have a
+    negative value to compensate for 'double counting' those features.
+
+    The interpretability of the contribution vector is open to debate. It
+    is not clear what statistical meaning the individual values have, but
+    in my tests they have reflected what appear to be 'real' feature effects.
+
+    Args:
+        x (ideally a pandas.DataFrame): The observation to consider
+        dist (pandas.DataFrame) : The distribution to take the distance from
+
+    Returns:
+        f (pandas.DataFrame): Feature contribution vector
+        m (float64): Mahalanobis distance squared. m is the sum of f.
+        p: The p-value calculated from m, assuming it follows a chi-square
+           distribution, and using the default degrees of freedom (dim(dist)-1) 
+    """
+
+    # hat-tip: https://www.machinelearningplus.com/statistics/mahalanobis-distance/
+    # NB: this produces the SQUARE of the distance ([x-m].C^{-1}.[x-m]^T)
+    # which is what we want if we're claiming that the chi-sq distribution applies
+
+    x_minus_mu = x - np.mean(dist)
+    cov = np.cov(dist.values.T)
+    inv_covmat = sp.linalg.inv(cov)
+    left_term = np.dot(x_minus_mu, inv_covmat)
+
+    # for the normal Mahalanobis distance we would take the dot product here
+    # but instead we multiply the vectors pointwise (as in .dot) but don't add
+    # up the entries. This lets us see how much each column contributes to the
+    # distance. NB I have NO IDEA what the statistical meaning of this is, since
+    # we've moved one vector into some weird space defined by the covariance matrix.
+    # It seems to have explanatory meaning, though.
+
+    v = left_term*x_minus_mu
+
+    m = np.dot(left_term, x_minus_mu.T)[0]
+    p = 1 - chi2.cdf(m, len(x.columns)-1)[0]
+
+    return (v, m[0], p)
+
+def chunk_explain(samp, dist, n=5000, chunksz=None, feats=la.ALL_FEATURES, seed=None):
+
+    """Take a sample and a distribution, and test the Mahalanobis distance
+    of the sample against a randomly sampled distribution drawn from dist.
+
+    The ideal chunk size is tricky to determine theoretically, but you can
+    just do empirical tests and see when your results start to stabilise.
+
+
+    Args:
+        samp (pandas.DataFrame): Sample (m rows x n features)
+        dist (pandas.DataFrame): Comparison distribution
+        n (int, default=5000): Number of samples to draw
+        seed (int, optional): seed the PRNG for the shuffling.
+                    ( for hackers, this is not passed directly to random.seed().
+                      Check the source for details )
+        feats (list, default=la.ALL_FEATURES): Features to use
+        chunksz (int, default=len(samp)): Chunk size to use
+
+    Returns (as per explain, but in all cases the mean):
+        f (pandas.Series): Feature contribution vector (sorted)
+        m (float64): Mahalanobis distance squared. m is the sum of f.
+        p: The p-value calculated from m, assuming it follows a chi-square
+           distribution, and using the default degrees of freedom (dim(dist)-1)     
+    """
+
+    s = samp[feats]
+    d = dist[feats]
+    if not chunksz:
+        chunksz = len(s)
+
+    rand_dist = _create_sampled_dist(d, chunksz, n, seed)
+    samp_centroid = la._chunk_mean(samp, len(samp))
+    v,m,p = explain(samp_centroid, rand_dist)
+    return( m, p, v.mean().sort_values(ascending=False) )
+
+def lazy_compare(samp, dist, n=5000, chunksz=None, feats=la.ALL_FEATURES):
+
+    """Print a quick comparison of a sample against a distribution,
+    using chunk_explain (cf).
+
+    Example Output:
+    ------------------------------
+    M-dist 41.65,  p-value: 0.0003
+    Feat     Score   Samp%   Dist%
+    ------------------------------
+    F2WC     14.39   0.00    11.55
+    BD       8.27    35.80   50.91
+    F1C      8.08    28.40   42.57
+    F4SC     3.36    66.67   60.11
+    F3C      2.73    87.65   84.27
+    F2SC     2.51    72.84   62.79
+    F3SC     2.32    79.01   81.82
+    F3WC     1.05    9.88    12.24
+    F4S      0.93    76.54   72.61
+    SYN      0.59    51.85   44.12
+    F4WC     0.39    6.17    4.88
+    F3S      0.33    59.26   61.34
+    F1S      0.23    43.21   50.66
+    F2S      -0.12   62.96   56.32
+    F4C      -1.68   64.20   60.71
+    F2C      -1.74   82.72   78.39
+    ------------------------------
+
+    Args (as per chunk_explain):
+        samp (pandas.DataFrame): Sample (m rows x n features)
+        dist (pandas.DataFrame): Comparison distribution
+        n (int, default=5000): Number of samples to draw
+        seed (int, optional): seed the PRNG for the shuffling.
+        feats (list, default=la.ALL_FEATURES): Features to use
+        chunksz (int, default=len(samp)): Chunk size to use
+
+    Returns:
+        Nothing (prints output)
+    """
+
+
+    m,p,f = chunk_explain(samp, dist, n, chunksz, feats)
+    samp_cent = la._chunk_mean(samp,len(samp))
+    dist_cent = la._chunk_mean(dist,len(dist))
+    print('-'*30)
+    print("M-dist %.2f,  p-value: %.4f" % (m,p))
+    print("Feat \t Score \t Samp% \t Dist%")
+    print('-'*30)
+    for feat,score in f.iteritems():
+        print("%s \t %.2f \t %.2f \t %.2f" % (feat, score, samp_cent[feat]*100, dist_cent[feat]*100))
+    print('-'*30)
+
+def _create_sampled_dist(dist, chunksz, distsz, seed=None):
+
+    """Take a lot of random samples from a binary distribution, each one of size
+    chunksz.
+
+    Eg: If we have a 10000 line poem, which has been converted to a binary distribution
+    using la.distribution, and we wanted 5000 random samples, with each one being the
+    mean of 150 random lines:
+
+    create_sampled_dist(poem_dist, 150, 5000)
+
+    Args:
+        dist (pandas.DataFrame): Source distribution, NOT chunked
+        chunksz (int): Chunk size for each observation
+        distsz (int): Number of observations to sample
+        seed (int, optional): seed the PRNG for the shuffling.
+
+    Returns:
+        (pandas.DataFrame): The final distribution
+    """
+
+    if chunksz > len(dist):
+        raise ValueError("Chunk size is bigger than source distribution.")
+
+    # This is much better than doing iterative pd.concat calls.
+    d = np.empty(distsz, dtype=object)
+    rng = np.random.RandomState(seed)
+    for x in range(distsz):
+        d[x] = la._chunk_mean(dist.sample(n=chunksz, random_state=rng), chunksz)
+    return pd.concat(d)
---
+++ mqdq/utils.py
@@ -0,0 +1,186 @@
+from bs4 import BeautifulSoup
+from collections import Counter
+import re
+import numpy as np
+import pandas as pd
+from mqdq import line_analyzer as la
+
+def grep(soup, s):
+
+    """Case insensitive grep on the text contents of bs4 soup object.
+
+    Args:
+        soup (bs4 soup): The text in which to search
+        s (string): String to search for. This will be converted
+                    into a regular expression, so re characters
+                    are allowed.
+
+    Returns:
+        (list): a list of matching bs4 <line>s
+    """
+
+    r = re.compile(s, re.IGNORECASE)
+    return list(set(s.parent.parent for s in soup.find_all(string=r)))
+
+
+def blat(ll, scan=True):
+
+    """Quickly print the text of a set of lines to screen
+
+    Args:
+        ll (list of bs4 <line>): Lines to print
+        scan (bool): if True, add scansion (see `txt`)
+    Returns:
+        Nothing (the lines are printed)
+    """
+
+    print("\n\n".join([txt(l, scan) for l in ll]))
+
+def txt(l, scan=False, number_with=None):
+
+    """Extract the text from a (single) line.
+
+    Args:
+        l (bs4 <line>): Line to operate on
+        scan (bool): If True, add a line of scansion and accent:
+                     Dixerat, atque illam media  inter talia   ferro
+                     1A'1b1c  2A'_  2T'3A 3b'3c_ 4A'4T 5A'5b5c 6A'6X
+
+        number_with (bs4 soup): If provided, text will be numbered by finding the lines
+                                in that bs4 object (WARNING! Can be slow!):
+                                8:196> Caede tepebat humus, foribusque affixa  superbis
+                                       1A'1b 1c2A'2b 2c'3A  3b3c4A'_   4T5A'5b 5c6A'6X WSQ
+    Returns:
+        (string): The result. Because of the internal de-duping, the results are not sorted.
+    """
+
+    res = []
+
+    try:
+            
+        words = l('word')
+
+        l_prefix = ''
+        scan_prefix = ''
+        if number_with:
+            l_prefix = bookref(l, number_with) + '> '
+            scan_prefix = ' '*len(l_prefix)
+
+        res=[]
+        if not scan:
+            return l_prefix + ' '.join([w.text for w in l('word')])
+        if l['pattern']=='corrupt' or l['pattern']=='not scanned':
+            return l_prefix + ' '.join([w.text for w in l('word')]) + "\n" + scan_prefix + "[corrupt]"
+
+        for w in words:
+            if len(w['sy'])==0:
+                if w.has_attr('mf') and w['mf']=='PE':
+                    # prodelision, the word is there but
+                    # the syllables vanished
+                    # Pollicita est -> Pollicit'est
+                    res.append(['_', w.text])
+                continue
+            syls = la._get_syls_with_stress(w)
+            if la._has_elision(w):
+                res.append([syls+'_',w.text])
+            else:
+                res.append([syls,w.text])
+            
+    except:
+        raise ValueError("Can't handle this: %s" % l)
+    
+    s1, s2='',''
+    # make the scan and the words line up, depending on which is longer.
+    for syls,txt in res:
+        if len(txt)>=len(syls):
+            s1 += txt + ' '
+            s2 += syls + ' '*(len(txt) - len(syls) + 1)
+        else:
+            s1 += txt + ' '*(len(syls) - len(txt) + 1)
+            s2 += syls + ' '
+    
+    return (l_prefix + s1.strip() + '\n' + scan_prefix + s2.strip())
+
+def txt_and_number(ll, every=5, scan=False, start_at=1):
+    
+    """Extract the text from a list of lines, with numbers. Where `txt` uses references to the text,
+    this uses independent numbers. Use this method if you want to print out a sequentially numbered
+    extract. Use `txt` if you want to add book refs to the result of a search or a random sample.
+
+    Args:
+        ll (list of bs4 <line>): Lines to operate on
+        every (int, default=5): How often to add numbers
+        start_at (int, default=1): Where to start the numbering
+
+    Returns:
+        (string): The result
+    """
+
+    # the string length of the highest line number (100==3)
+    n_len = len(str(len(ll)+start_at))
+    strs = [txt(l,scan) for l in ll]
+    numbered = []
+    for idx, s in enumerate(strs):      
+        if scan:
+            s1, s2 = s.splitlines()
+            if (idx+start_at)%every==0:
+                s1 = ("%*d  " % (n_len,idx+start_at)) + s1
+            else:
+                s1 = ' '*(n_len+2) + s1
+            s2 = ' '*(n_len+2) + s2
+            numbered.append('\n'.join([s1,s2]))
+        else:
+            if (idx+start_at)%every==0:
+                s = ("%*d  " % (n_len,idx+start_at)) + s
+            else:
+                s = ' '*(n_len+2) + s
+            numbered.append(s)
+
+    return numbered
+
+def which_book(l, soup):
+
+    """Determine which book in a bs4 soup contains a given line.
+
+    Args:
+        l (bs4 <line>): Line to check
+        soup (bs4 soup): Soup to check in
+
+    Returns:
+        (int): The index of the 'division' containing l (0-based) or None
+    """
+
+    for d in soup('division'):
+        if l in d:
+            return d['title']
+    return None
+
+def bookref(l, soup):
+
+    """Return a formatted reference book:line for a given line and text.
+
+    Args:
+        l (bs4 <line): Line to use
+        soup (bs4 soup): Text to use for the numbering
+
+    Returns:
+        (string): The reference, eg 6:825 or None
+    """
+
+    b = which_book(l, soup)
+    if not b:
+        return None
+    return("%2s:%-3d" % (b, int(l['name'])))
+
+def clean(ll):
+
+    """Remove all corrupt lines from a set of bs4 <line>s
+
+    Args:
+        ll (list of bs4 <line>): Lines to clean
+
+    Returns:
+        (list of bs4 <line>): The lines, with the corrupt ones removed.
+    """
+
+    return [l for l in ll if l['pattern']!='corrupt' and l['pattern']!='not scanned']
