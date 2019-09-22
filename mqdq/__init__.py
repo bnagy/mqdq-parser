8b949a12605380c62c266c4f7af7a93eb00b13f8
ben <git@ben.iagu.net>
2019-09-22 15:48:06 +0930

    fix import bug

--- mqdq/__init__.py
+++ mqdq/__init__.py
@@ -2,6 +2,4 @@ from . import line_analyzer
 from . import mahalanobis
 from . import utils
 from . import rhyme
-from cltk_hax import syllabifier
-from cltk_hax import string_utils
-from cltk_hax import scansion_constants
+from . import cltk_hax
