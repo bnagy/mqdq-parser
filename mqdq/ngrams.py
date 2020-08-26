763e38df9114594ee4ffe6c95f7e95793d964970
ben <git@ben.iagu.net>
2020-08-26 19:01:42 +0930

    make named entity trimming configurable

--- mqdq/ngrams.py
+++ mqdq/ngrams.py
@@ -7,11 +7,12 @@ from mqdq import utils
 from mqdq import rhyme
 from mqdq import line_analyzer as la
 import string
+import math
 
 DEPUNCT = str.maketrans('', '', string.punctuation)
 DEFANCY = str.maketrans({'ü':'y', u'\u0304':None, u'\u0303':None, '`':None})
 
-def _remove_propers(ll, type='phon'):
+def _remove_propers(ll, type='phon', min_proper_len=4):
 
 	fixed_lines = []
 	propers = set()
@@ -41,7 +42,7 @@ def _remove_propers(ll, type='phon'):
 				pre.startswith(("'",'"'))):
 
 				# don't learn very short proper nouns
-				if len(w) >= 5:
+				if len(w) >= min_proper_len:
 					propers.add(w)
 					if w.endswith('_'):
 						this_line.append('8_')
@@ -62,7 +63,7 @@ def _remove_propers(ll, type='phon'):
 				else:
 					l[i] = ('8')
 
-	return fixed_lines
+	return [_join_line_array(l) for l in fixed_lines]
 
 def _join_line_array(ary):
 	s = ary[0]
@@ -92,9 +93,7 @@ def _just_stringify(ll, type='orth'):
 
 		fixed_lines.append([x[1].lower() for x in a])
 
-	return fixed_lines
-
-
+	return [_join_line_array(l) for l in fixed_lines]
 
 def common_wordstems(ll, max_stem=5):
 
@@ -123,47 +122,94 @@ WORKS = [
 	('Ovid', 'Amores3', 'ovid/OV-amo3.xml')
 ]
 
-def string_chunks(ll, chunksz=80, type='phon', drop_propers=True):
+def string_chunks(ll, chunksz=80, type='phon', drop_propers=True, min_proper_len=4):
 	if drop_propers:
-		ll = _remove_propers(ll, type=type)
+		ll = _remove_propers(ll, type=type, min_proper_len=min_proper_len)
 	else:
-		ll = _just_stringify(ll, type=type)
+		ll = _just_stringify(ll, type=type, min_proper_len=min_proper_len)
 
 	chunks = list(la._chunks(ll, chunksz))
 	if len(ll)%chunksz != 0:
 		chunks = chunks[:-1]
 
-	chunks = [[_join_line_array(x) for x in c] for c in chunks]
 	chunks = [''.join(ss) for ss in chunks]
 	return chunks
 
-def geezit_corpus(chunksz=80, type='phon', drop_propers=True):
 
-	df = pd.DataFrame()
+def geezit_corpus(chunksz=80, type='phon', drop_propers=True, drop_addit=True, min_proper_len=4):
 
-	for auth,title,fn in WORKS:
-		
-		with open(fn) as fh:
-			soup = BeautifulSoup(fh,"xml")
-		ll = utils.clean(soup('line'))
-		if title=='Punica':
-			print("Cleaning out the Additamentum. %d lines" % len(ll))
-			puni_books = [utils.clean(d('line')) for d in soup('division')]    
-			addit=puni_books[7][143:224]
-			ll = [l for l in ll if l not in addit]
-			print("OK, %d lines left." % len(ll))
+    df = pd.DataFrame()
 
-		tmpdf = pd.DataFrame()
-		try:
-			chunks = string_chunks(ll, chunksz, type, drop_propers)
-		except Exception as e:
-			print("Failed on %s." % title)
-			raise e
-		tmpdf['Chunk'] = chunks
-		tmpdf['Author'] = auth
-		tmpdf['Work'] = title
-		df = df.append(tmpdf).reset_index(drop=True)
+    for auth,title,fn in WORKS:
 
-	return df
+        with open(fn) as fh:
+            soup = BeautifulSoup(fh,"xml")
+        ll = utils.clean(soup('line'))
+        
+        if title=='Punica' and drop_addit:
+            
+            # Handle the Punica stuff manually.
+            
+            tmpdf = pd.DataFrame()
+                # We need to first remove the propers and then chunk manually
+                # because drop_propers uses capitalisation data from
+                # the whole text
+            if drop_propers:
+                ll = _remove_propers(ll, type=type, min_proper_len=min_proper_len)
+            else:
+                ll = _just_stringify(ll, type=type)
+                    
+            puni_na = ll[:5230] + ll[5311:]
+            addit = ll[5230:5311]
+            if len(addit) + len(puni_na) != len(ll):
+                raise RuntimeError("lrn to slice :(")
+                
+            puni_chunks = list(la._chunks(puni_na, chunksz))
+            puni_chunks = [''.join(ss) for ss in puni_chunks]
+
+            if len(puni_na)%chunksz != 0:
+                puni_chunks = puni_chunks[:-1]
+                
+            addit_chunk = ''.join(addit)
+            
+            # which chunk contains the last line _before_ the addit?
+            # it will have stolen from 0 to chunksz-1 lines of the following chunk
+            last_good_chunk = int(math.ceil(5230/chunksz))
+            # these indices haven't been messed with
+            good_indices = np.arange(last_good_chunk)*chunksz + 1
+            # these indices start from the line that ends last_good_chunk
+            # (which is the chunk that stole some lines)
+            shifted_indices = np.arange(last_good_chunk, len(puni_chunks))*chunksz + chunksz
+            puni_refs = ["%d:%d" % tupl for tupl in utils.indices_to_bookref(soup, np.concatenate((good_indices,shifted_indices)))]
+            # build the Punica temp frame
+            tmpdf['Chunk'] = puni_chunks
+            tmpdf['Author'] = auth
+            tmpdf['Work'] = title
+            tmpdf['Bookref'] = puni_refs
+            # manually add an entry for the Addit
+            addit_df = (pd.DataFrame(
+                [{'Chunk':addit_chunk,
+                  'Author':'Unknown',
+                  'Work':'Punica',
+                  'Bookref':'8:144'}]
+            ))
+            tmpdf = tmpdf.append(addit_df).reset_index(drop=True)
+            # append the whole lot to the final frame
+            df = df.append(tmpdf).reset_index(drop=True)
+            continue
+
+        tmpdf = pd.DataFrame()
+        try:
+            chunks = string_chunks(ll, chunksz, type, drop_propers, min_proper_len=min_proper_len)
+        except Exception as e:
+            print("Failed on %s." % title)
+            raise e
+        tmpdf['Chunk'] = chunks
+        tmpdf['Author'] = auth
+        tmpdf['Work'] = title
+        tmpdf['Bookref'] = ["%d:%d" % tupl for tupl in utils.indices_to_bookref(soup, (np.arange(len(chunks))*chunksz)+1)]
+        df = df.append(tmpdf).reset_index(drop=True)
+
+    return df
 
 
