d1721f2dd2a5b01e816a756876fc88471b52948b
ben <git@ben.iagu.net>
2022-01-17 10:46:12 +1030

    fixed a packaging bug

--- setup.py
+++ setup.py
@@ -3,25 +3,25 @@ from distutils.core import setup
 import os
 
 setup(
-	name='MQDQParser',
-	version='0.5.0',
-	author='Ben Nagy',
-	packages=['mqdq'],
-	license='3-Clause BSD',
-	url='https://github.com/bnagy/mqdq-parser',
-	long_description='''
+    name="MQDQParser",
+    version="0.6.1",
+    author="Ben Nagy",
+    packages=["mqdq", "mqdq.cltk_hax"],
+    license="3-Clause BSD",
+    url="https://github.com/bnagy/mqdq-parser",
+    long_description="""
 	mqdq is a collection of utility and analysis 
 	code for working with the XML format of scanned 
 	Latin poems provided by MQDQ. These XML files 
 	can be downloaded from http://www.pedecerto.eu/pagine/autori
-	''',
-	install_requires=[
-		'setuptools',
-		'beautifulsoup4',
-		'numpy',
-		'scipy',
-		'pandas',
-		'seaborn',
-		'dominate'
-	],
+	""",
+    install_requires=[
+        "setuptools",
+        "beautifulsoup4",
+        "numpy",
+        "scipy",
+        "pandas",
+        "seaborn",
+        "dominate",
+    ],
 )
